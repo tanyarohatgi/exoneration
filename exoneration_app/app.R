50e1b6388b449af5ab1f1b759fc7204622d95268
Tanya Rohatgi <tanyarohatgi@Tanyas-MacBook-Pro-New.local>
2018-12-15 20:26:02 -0500

    Comments added.

--- exoneration_app/app.R
+++ exoneration_app/app.R
@@ -1,12 +1,3 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(tidyverse)
 library(shiny)
 library(ggplot2)
@@ -15,13 +6,26 @@ library(stringr)
 library(janitor)
 library(plotly)
 library(styler) 
-#I used the Styler addin to format my code in accordance with the tidyverse styleguide. 
 
-# ADDITIONAL DATA CLEANING/MANIPULATING:
+#I used the Styler addin to format my code in accordance with the tidyverse
+#styleguide.
+
+### IMPORTING DATA USING AN .RDS FILE: ###
 
 by_crime1 <- read_rds("by_crime.rds")
 
+### ADDITIONAL DATA CLEANING/MANIPULATING: ###
+
 by_crime1$crime_type <- as.factor(by_crime1$crime_type)
+by_crime1$race <- as.factor(by_crime1$race)
+
+by_crime1 <- by_crime1 %>%
+  mutate(race = fct_relevel(race, c("Other", "Native American", "Asian", "Hispanic", "Black", "White")))
+
+# Adding the crime types that — because of their unique nature/what I'm trying
+# to explore — have not been added to the violent/non-violent/vary-by-case
+# categories, to a new 'Other' category for cleaner visualisation in subsetted
+# plots:
 
 by_crime2 <- by_crime1 %>%
   mutate(crime_type2 = case_when(
@@ -33,6 +37,17 @@ by_crime2 <- by_crime1 %>%
   ))
 
 by_crime2$crime_type2 <- as.factor(by_crime2$crime_type2)
+
+# Creating a new dataframe with the names of basis-of-exoneration tags as
+# drop-down options I'll need later, which will map onto the same tag names in
+# the main dataframe (the preparation for which is the re-naming of the tags
+# from the dataframe by_crime1 to the new dataframe by_crime_new): I do this in
+# order to get the tag names in the drop-down at all, because in many cases
+# multiple tags apply to a single observation, and I choose to look at them
+# individually. Using the main dataframe to do so would require a reactive
+# function because each tag must be filtered individually, which is not possible
+# in the input section of the ui. Therefore a new dataframe with just the tag
+# names I need must be created:
 
 x <- c("DNA", "FC", "MWID", "F/MFE", "P/FA", "OM", "ILD")
 new <- as.data.frame(x)
@@ -63,6 +78,8 @@ by_crime_new <- by_crime1 %>%
 new1$x <- as.factor(new1$x)
 by_crime_new$basis <- as.factor(by_crime_new$basis)
 
+# Here I create the 3 time periods I will use to subset my later plot on CIUs:
+
 by_crime3 <- by_crime2 %>%
   mutate(exonerated = case_when(
     exonerated >= 2013 & exonerated <= 2018 ~ "2013 - Present",
@@ -75,25 +92,35 @@ by_crime3$county <- as.factor(by_crime3$county)
 by_crime3$race <- as.factor(by_crime3$race)
 
 
-# APP CODE:
+### APP CODE: ###
 
 ui <- fluidPage(
   theme = shinytheme("cosmo"),
+  
+  br(),
+  
+  # Using html to format the header, add a favicon, and add and format a
+  # background image for the 'About this Project' tab:
 
   tags$head(
     tags$style(
-      ".title {margin: auto; width: 500px; text-align:center; font-size : 60px}"
+      ".title {margin: auto; width: 600px; text-align:center; font-size : 30px}"
     )
   ),
 
-  tags$div(class = "title", titlePanel(strong("EXPLORING EXONERATION DATA"))),
+  tags$div(class = "title", titlePanel(strong("EXPLORING U.S. EXONERATION DATA"), windowTitle = "Exonerations in the U.S.")),
   h4("TANYA ROHATGI", align = "center"),
+  
+  tags$head(
+    tags$link(rel = "shortcut icon", href = "https://s3.amazonaws.com/user-media.venngage.com/416105-88b87a96f427878faa56ce057443dd56.png")),
 
   tags$head(
     tags$style(HTML(".background{ width: 100%; background-size: 100% 116%; text-align:center; position: absolute; left: 0;
                                   background-image: url(https://cdn-images-1.medium.com/max/2000/1*N36dU-o02IRwRoqQVZqqdg.jpeg);
                                   }"))
   ),
+  
+  # Adding tabs and content:
 
   tabsetPanel(
     tabPanel(
@@ -110,7 +137,7 @@ ui <- fluidPage(
         h4(strong("Take a look under the hood:")),
         h4("Access the code for this app", tags$a(href = "https://github.com/tanyarohatgi/exoneration", "here."), "Browse my other work this past semester for Harvard College's 'GOV 1005: Data'", tags$a(href = "https://github.com/tanyarohatgi", "here.")),
         br(), br(), br(), br(), br(), br(), br(), br(),
-        br(), br(), br(), br(), br(), br(), br(), br(), br()
+        br(), br(), br(), br(), br(), br(), br(), br()
       )
     ),
 
@@ -118,7 +145,7 @@ ui <- fluidPage(
       "Plots and Findings",
       br(),
       tabsetPanel(
-        type = "pills",
+        type = "pills", 
         tabPanel(
           "Introductory Plots",
 
@@ -137,13 +164,14 @@ ui <- fluidPage(
 
               helpText("NOTES:"),
               helpText("1. The type-of-crime chart details only the worst crime the exoneree was convicted for, and does not include any other crimes/misdemeanors they may have committed."),
-              helpText("2. The gender of the exoneree refers to their gender at the time of conviction.")
+              helpText("2. The gender of the exoneree refers to their gender at the time of conviction."),
+              helpText("3. Vary-by-Case crimes are those that, depending on the individual circumstances of the case, could be either violent or non-violent. Examples include harrassment and supporting terrorism.")
             ),
 
             mainPanel(
               h4(strong("I. Exonerations in the United States between 1989 and 2018:")),
               h5("Breakdowns by type of crime and demograhic characteristics."),
-              plotOutput("PlotA"),
+              plotlyOutput("PlotA"),
               p(""),
               p("Type of Crime: While we see the consistent presence of violent offense convictions through the years, there has been a recent spike in the percentage of exonerations for non-violent and drug-related offence convictions.
                             This is somewhat surprising; it would be reasonable to expect that scare re-consideration resources would be spent on high-profile cases — such as of those on death row — where the crimes committed were most likely violent.",
@@ -199,7 +227,6 @@ ui <- fluidPage(
           ),
 
           br(),
-          br(),
 
           sidebarLayout(
             sidebarPanel(
@@ -209,7 +236,7 @@ ui <- fluidPage(
                 choices = levels(by_crime2$crime_type2)
               ),
 
-              helpText("Hover over the barplot to identify state and exoneration count.")
+              helpText("Hover over the barplot to identify race and exoneration count.")
             ),
 
 
@@ -239,7 +266,7 @@ ui <- fluidPage(
 
             mainPanel(
               h4(strong("IV. Number of Exonerations Secured by Conviction Integrity Units in Prosecutorial Offices Over Time:")),
-              plotOutput("PlotD"),
+              plotlyOutput("PlotD"),
               p("As we see above, CIUs play no definitive role in exonerations before 2003. Given this we can conclude that they only began to come into existence around this time. As we can see in both Gaph IV above and Graph V below, their involvement has grown rapidly in the years following; from only 2 exonerations secured between 2003 and 2007, to 21 between 2008 and 2012, to 266 between 2013 and now. This indicates a growing (but by no means sufficient, because only 12 states have CIUs that have resulted in even a single exoneration) degree of comprehension in prosecutorial offices that wrongful convictions are a serious and frequent problem, one that the office itself must play a role in remedying.",
                 style = "font-size : 11pt"
               ),
@@ -263,14 +290,15 @@ ui <- fluidPage(
                 choices = levels(by_crime3$exonerated)
               ),
 
-              checkboxInput("county", label = "Fill by county instead of crime type.")
+              checkboxInput("county", label = "Fill by county instead of crime type."),
+              helpText("For the by-county plot: Since there are too many counties (especially 2013 onwards) for a legend to be helpful or visually significant, hover over the barplot to identify county and exoneration count instead.")
             ),
 
 
             mainPanel(
               h4(strong("V. Number of Exonerations Secured by Conviction Integrity Units in Prosecutorial Offices:")),
               h5("Data can be viewed in 5 year increments, beginning in 2003 when CIU exonerations are first seen."),
-              plotOutput("PlotE"),
+              plotlyOutput("PlotE"),
               br()
             )
           )
@@ -293,8 +321,8 @@ ui <- fluidPage(
             mainPanel(
               h4(strong("VI. Basis for Exonerations, by Race:")),
               h5("Comparative view of total exonerations and those based, at least in part, on specific mistakes/misconduct during trial."),
-              plotOutput("PlotF"),
-              p("Some key take-aways from this plot: official misconduct, wherein police, prosecutors, or other officials abused their power or the workings of the judicial process, was a contributing factor in over half the convictions of exonerees of all races but Asian.
+              plotlyOutput("PlotF"),
+              p("Some key take-aways from this plot: official misconduct, wherein police, prosecutors, or other officials abused their power or the workings of the judicial process, was a contributing factor in over half the convictions of exonerees of all races but Asian and Other.
                              This is also true for perjury or false accusations. However mistaken witness ID, wherein one or more witness confirmed — mistakenly — that they saw the exoneree commit the crime, sent more black exonerees to jail than those of any other race.",
                 style = "font-size : 11pt"
               )
@@ -308,93 +336,183 @@ ui <- fluidPage(
 
 
 
-# Define server logic required to draw a histogram
 server <- function(input, output) {
-  output$PlotA <- renderPlot({
-    by_crime1 %>%
+  output$PlotA <- renderPlotly({
+    
+    # In all my plots, I have done away with axis labels and legend labels for a
+    # neater look. I can do this without impacting the effectiveness of the
+    # visualisation because 1) often the varaibles on the axis are self-evident,
+    # such as counts in barplots, and 2) they are always made explicit both in
+    # the plot title and subtitle, and in the details/instructions in the
+    # sidebarPanels.
+    
+    ply0 <- by_crime1 %>%
       ggplot(aes_string("exonerated")) + geom_bar(aes_string(fill = input$a), width = 0.9) + theme_minimal() +
       theme(legend.position = "bottom") +
       xlab("") + ylab("") + labs(fill = "")
+    
+    # In the plots where I keep the legend, I make them hozizonal and position
+    # them at the bottom of the plots using plotly functions and arguments, such
+    # as below:
+    
+    ply0 %>%
+      ggplotly(tooltip = "count") %>%
+      layout(legend = list(orientation = "h",  
+                           xanchor = "center",
+                           x = 0.5,
+                           y = -0.1, 
+                           traceorder = "reversed",
+                           font = list(family = "sans-serif", size = 11))) %>%
+      style(legendgroup = NULL)
+    
+    # The 'legendgroup = NULL' argument allows the elements of the now
+    # horizontal legend to wrap into multiple rows if needed, instead of showing
+    # only the number that fit in one row. There is no dedicated argument to
+    # manually set rows yet.
+    
   })
 
-  # The next 2 plots use plotly. I'm not typically a fan of plotly, because I think the formatting is inelegant, but as I explain in the sidebarPanel helpText, there are too many states in PlotB for a legend to be helpful, and so a hover works. I've done the same to PlotC for consistency in the tab.
-
   output$PlotB <- renderPlotly({
+    
+    # In many of the plots I mutate variable names to title cases ("variable" to
+    # "Variable"), for the purpose of tooltip display:
+    
     ply <- by_crime2 %>%
       mutate(crime = worst_crime_display) %>%
       filter(crime_type2 == input$b) %>%
-      ggplot(aes_string("crime")) + geom_bar(aes_string(fill = "state")) +
-      theme(legend.position = "bottom") + theme_minimal() + xlab("") + ylab("") + labs(fill = "") +
-      coord_flip() + theme(legend.position = "bottom", legend.key.size = unit(0.17, "cm")) +
-      guides(fill = guide_legend(nrow = 6, byrow = TRUE))
+      mutate(State = state) %>%
+      ggplot(aes_string("crime")) + geom_bar(aes_string(fill = "State")) +
+      theme(legend.position = "bottom") + theme_minimal() + 
+      xlab("") + ylab("") + labs(fill = "") +
+      coord_flip() 
+    
+    # I remove the legend because there are too many states displayed, and so a
+    # plotly hover by itself works better:
 
     ply %>%
-      ggplotly(tooltip = c("state", "count")) %>%
+      ggplotly(tooltip = c("State", "count")) %>%
       layout(showlegend = F)
+    
   })
 
   output$PlotC <- renderPlotly({
+    
     ply2 <- by_crime2 %>%
       mutate(crime = worst_crime_display) %>%
       filter(crime_type2 == input$c) %>%
-      ggplot(aes_string("crime")) + geom_bar(aes_string(fill = "race")) +
-      theme(legend.position = "bottom") + theme_minimal() + xlab("") + ylab("") + labs(fill = "") +
-      coord_flip() + theme(legend.position = "bottom", legend.key.size = unit(0.17, "cm")) +
-      guides(fill = guide_legend(nrow = 1, byrow = TRUE))
+      mutate(Race = race) %>%
+      ggplot(aes_string("crime")) + geom_bar(aes_string(fill = "Race")) +
+      theme(legend.position = "bottom") + theme_minimal() + 
+      xlab("") + ylab("") + labs(fill = "") +
+      coord_flip() 
 
     ply2 %>%
-      ggplotly(tooltip = c("race", "count")) %>%
-      layout(showlegend = F)
+      ggplotly(tooltip = c("Race", "count")) %>%
+      layout(legend = list(orientation = "h",  
+                           xanchor = "center",
+                           x = 0.5,
+                           y = -0.1, 
+                           traceorder = "reversed",
+                           font = list(family = "sans-serif", size = 11)))
+      
   })
 
 
-  output$PlotD <- renderPlot({
+  output$PlotD <- renderPlotly({
+    
     if (input$total == "Total CIU Exonerations") {
-      by_crime1 %>%
+      
+      ply3 <- by_crime1 %>%
         mutate(tags = str_extract(tags, "CIU")) %>%
         filter(tags == "CIU") %>%
         group_by(exonerated, state) %>%
         count(tags) %>%
-        ggplot(aes_string("exonerated")) + geom_line(aes_string(y = "n"), size = 1.5, alpha = 0.8, color = "orchid") +
+        mutate(Count = n) %>%
+        mutate(Exonerated = exonerated) %>%
+        ggplot(aes_string("Exonerated")) + geom_line(aes_string(y = "Count"), size = 0.8, color = "orchid") +
         theme_minimal() +
         xlab("") + ylab("")
+      
+      ply3 %>%
+        ggplotly(tooltip = c("Exonerated", "Count")) 
     }
 
     else {
-      by_crime1 %>%
+      
+      ply4 <- by_crime1 %>%
         mutate(tags = str_extract(tags, "CIU")) %>%
         filter(tags == "CIU") %>%
         group_by(exonerated, state) %>%
         count(tags) %>%
-        ggplot(aes_string("exonerated")) + geom_line(aes_string(y = "n", color = "state"), size = 1.5, alpha = 0.8) + theme_minimal() +
+        mutate(Count = n) %>%
+        mutate(Exonerated = exonerated, State = state) %>%
+        ggplot(aes_string("Exonerated")) + geom_line(aes_string(y = "Count", color = "State"), size = 0.8) + 
+        theme_minimal() +
         xlab("") + ylab("") + labs(color = "")
+      
+      ply4 %>%
+      ggplotly(tooltip = c("State", "Exonerated", "Count")) %>%
+        layout(legend = list(font = list(family = "sans-serif", size = 11)))
+      
     }
   })
 
-  output$PlotE <- renderPlot({
+  output$PlotE <- renderPlotly({
+    
     if (input$county == TRUE) {
-      by_crime3 %>%
+      
+      ply5 <- by_crime3 %>%
         mutate(tags = str_extract(tags, "CIU")) %>%
         filter(tags == "CIU") %>%
         select(-basis) %>%
         filter(exonerated == input$e) %>%
-        ggplot(aes_string("state")) + geom_bar(aes_string(fill = "county")) + theme_minimal() +
-        xlab("") + ylab("") + labs(fill = "County") + coord_flip() + theme(legend.position = "bottom", legend.key.size = unit(0.32, "cm"))
+        mutate(County = county) %>%
+        ggplot(aes_string("state")) + geom_bar(aes_string(fill = "County")) + 
+        theme_minimal() +
+        xlab("") + ylab("") + labs(fill = "County") + 
+        coord_flip() 
+       
+      # Once again, removing legend because there are too many counties for a
+      # legend to be helpful. Using only the plotly hover instead:
+      
+      ply5 %>% 
+        ggplotly(tooltip = c("County", "count")) %>%
+        layout(showlegend = F)
+
     }
 
     else {
-      by_crime3 %>%
+      
+      ply6 <- by_crime3 %>%
         mutate(tags = str_extract(tags, "CIU")) %>%
         filter(tags == "CIU") %>%
         select(-basis) %>%
         filter(exonerated == input$e) %>%
-        ggplot(aes_string("state")) + geom_bar(aes_string(fill = "crime_type")) + theme_minimal() +
-        xlab("") + ylab("") + labs(fill = "Crime Type") + coord_flip() + theme(legend.position = "bottom")
+        mutate(Category = crime_type) %>%
+        ggplot(aes_string("state")) + geom_bar(aes_string(fill = "Category")) + 
+        theme_minimal() +
+        xlab("") + ylab("") + labs(fill = "Crime Type") + 
+        coord_flip() 
+      
+      ply6 %>% 
+        ggplotly(tooltip = c("Category", "count")) %>%
+        layout(legend = list(orientation = "h",  
+                             xanchor = "center",
+                             x = 0.5,
+                             y = -0.1, 
+                             font = list(family = "sans-serif", size = 10)))
     }
   })
 
-  output$PlotF <- renderPlot({
+  output$PlotF <- renderPlotly({
+    
+    # For this plot, I use multiple reactive functions in order to first create
+    # a reactive object wherein the dataframe is filtered for individual
+    # basis-of-exoneration tags, and then to work with this object (which can
+    # only be done in a reactive context):
+    
     t4 <- reactive({
+      
       by_crime_new %>%
         mutate(b = ifelse(str_detect(basis, input$f), T, F)) %>%
         filter(b == T) %>%
@@ -406,18 +524,32 @@ server <- function(input, output) {
 
 
     t7 <- reactive({
+      
       left_join(t5, t4(), by = "race") %>%
         transmute(race = race, Total = n.x, Based = n.y) %>%
-        gather("count.type", "value", Total, Based)
+        gather("count.type", "Value", Total, Based)
     })
 
 
-    t7() %>%
-      ggplot(aes(race, value)) + geom_bar(aes(fill = count.type), stat = "identity", position = "dodge") +
-      xlab("") + ylab("") + theme_minimal() + scale_fill_manual("", values = c("Total" = "royalblue1", "Based" = "orchid1")) +
+    ply7 <- reactive({ 
+      
+      t7() %>%
+      ggplot(aes(race, Value)) + geom_bar(aes(fill = count.type), stat = "identity", position = "dodge") +
+      xlab("") + ylab("") + theme_minimal() + 
+      scale_fill_manual("", values = c("Total" = "royalblue1", "Based" = "orchid1")) +
       coord_flip()
+      
+    })
+    
+      ply7() %>%
+      ggplotly(tooltip = c("Value")) %>%
+      layout(legend = list(orientation = "h",  
+                           xanchor = "center",
+                           x = 0.5,
+                           y = -0.1, 
+                           font = list(family = "sans-serif", size = 11)))
   })
 }
 
-# Run the application
+
 shinyApp(ui = ui, server = server)
